5836554ca086e6572c1e7f51c06f1f6046e10f07
mthoma <mthoma@auto.tuwien.ac.at>
2023-08-29 16:54:14 +0200

    influx db integration

--- simulators/simulation-configs.js
+++ simulators/simulation-configs.js
@@ -3,7 +3,7 @@ export default {
     {
       name: 'Household 1',
       pvs: [{
-        file: './production_values/pv_sim_export_4000_0_45.json',
+        file: './production_values/pv_sim_export_6000_180_45.json',
         port: 9001
       }],
       evs: [{
@@ -24,16 +24,16 @@ export default {
         dischargingEfficiency: 0.92,
         port: 9004
       }],
-      consumption: [{
+      consumptions: [{
         file: 'consumption_values/House_1-HH_1.json',
         port: 9005
       }],
-      influx: [{
+      influx: {
         url: "http://localhost:8086",
         bucket: "sim",
-        token: "IwNUpz2WP7Mzz3enDoPkckFAJVaw1O6vBT3QOj0STk9P5KSUZkwVYkHyWahzML7RDzFApm6xjdoX3VoK80G0Lg==",
+        token: "zkF_MBAO4dcMB65cSmgz8LBmow7dUYa38iQtXmo9mAOR6Yw4oEGINN1I8PFNapL-3E4tNR9ABSZIT5XhZjcLyQ==",
         org: "home"
-      }]
+      }
     }
   ]
 };
--- simulators/simulator.js
+++ simulators/simulator.js
@@ -4,66 +4,74 @@ import config from './simulation-configs.js';
 import Battery from './battery-simulator.js';
 import PV from './pv-simulator.js';
 import Consumption from './consumption-simulator.js';
+import InfluxWrite from "./influx-write.js";
 
-const { households: householdsConfig } = config; 
+(async () => {
+    const {households: householdsConfig} = config;
 
-const households = [];
+    const households = [];
 
 // setup all households from config file
-for (const householdConfig of householdsConfig) {
-    
-    const household = {};
+    for (const householdConfig of householdsConfig) {
 
-    household.consumptions = [];
-    for (const consumptionConfig of householdConfig.consumptions) {
-        const consumption = new Consumption(consumptionConfig);
-        household.consumptions.push(consumption);
+        const household = {};
+
+        household.infux = new InfluxWrite(householdConfig.influx)
+
+        household.consumptions = [];
+        for (const consumptionConfig of householdConfig.consumptions) {
+            const consumption = new Consumption(consumptionConfig);
+            household.consumptions.push(consumption);
+        }
+
+        household.pvs = [];
+        for (const pvConfig of householdConfig.pvs) {
+            const pv = new PV(pvConfig);
+            household.pvs.push(pv);
+        }
+
+        household.batteries = [];
+        for (const batteryConfig of householdConfig.batteries) {
+            const battery = new Battery(batteryConfig);
+            household.batteries.push(battery);
+        }
+        households.push(household)
     }
 
-    household.pvs = [];
-    for (const pvConfig of householdConfig.pvs) {
-        const pv = new PV(pvConfig);
-        household.pvs.push(pv);
-    }
-
-    household.batteries = [];
-    for (const batteryConfig of householdConfig.batteries) {
-        const battery = new Battery(batteryConfig);
-        household.batteries.push(battery);
-    }
-
-    households.push(household);
-}
-
-const simulationTime = moment('2021-09-01T00:00:00');
+    const simulationTime = moment('2021-09-01T00:00:00');
 
 // simulation loop
-for (let i = 0; i < 7 * 24 * 60; i++) {
-    simulationTime.add(60, 'seconds');
+    for (let i = 0; i < 7 * 24 * 60; i++) {
+        simulationTime.add(60, 'seconds');
 
-    console.log(simulationTime.toDate());
+        // console.log(simulationTime.toDate());
 
-    for (const household of households) {
-        let residualEnergyInKWh = 0;
+        for (const household of households) {
+            let residualEnergyInKWh = 0;
+            // consumption and pv production are fixed and can not be changed
+            // for this reason, we get the consumption and production data first and secondly give the battery the chance to charge
+            // this strategy neglects the ev charging at first and priorizes the battery charging
+            // the ev charging strategy is the job of evcc
 
-        // consumption and pv production are fixed and can not be changed
-        // for this reason, we get the consumption and production data first and secondly give the battery the chance to charge
-        // this strategy neglects the ev charging at first and priorizes the battery charging
-        // the ev charging strategy is the job of evcc
+            for (const consumption of household.consumptions) {
+                residualEnergyInKWh += consumption.update(60);
+                const consumptionPower = consumption.getCurrentPower();
+                await household.infux.updateDB("sm1", "Smart_Meter_Reading", "power", consumptionPower, simulationTime.toDate())
 
-        for (const consumption of household.consumptions) {
-            residualEnergyInKWh += consumption.update(60);
-            const consumptionPower = consumption.getCurrentPower();
-        }
+            }
 
-        for (const pv of household.pvs) {
-            residualEnergyInKWh += pv.update(60);
-            const pvPower = pv.getCurrentPower();
-        }
+            for (const pv of household.pvs) {
+                residualEnergyInKWh += pv.update(60);
+                const pvPower = pv.getCurrentPower();
+                await household.infux.updateDB("pv1", "PV_Inverter_Reading", "power", pvPower, simulationTime.toDate())
 
-        for (const battery of household.batteries) {
-            residualEnergyInKWh += battery.update(60);
-            const batteryPower = battery.getCurrentPower();
+
+            }
+
+            for (const battery of household.batteries) {
+                residualEnergyInKWh += battery.update(60);
+                const batteryPower = battery.getCurrentPower();
+            }
         }
     }
-}
+})()
