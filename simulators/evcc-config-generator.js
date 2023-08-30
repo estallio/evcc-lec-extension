91a64295d845f08baf4d7279f99fa1a9f4d948a1
mthoma <mthoma@auto.tuwien.ac.at>
2023-08-30 22:42:25 +0200

    generation of multiple evcc config files

--- simulators/evcc-config-generator.js
+++ simulators/evcc-config-generator.js
@@ -1,202 +1,207 @@
 import yaml from 'js-yaml';
 import config from './simulation-configs.js';
+import fs from 'fs';
 
 
 const {households: householdsConfig} = config;
 
 for (const household of householdsConfig) {
-  const evccConfig = {};
+    const evccConfig = {};
 
-  evccConfig.log = "debug"
-  evccConfig.interval = "1s"
+    evccConfig.log = "debug"
+    evccConfig.interval = "1s"
 
-  evccConfig.site = {
-    title: household.name, meters: {
-      grid: "meter1", pv: "pv1", battery: "battery1"
+    evccConfig.site = {
+        title: household.name, meters: {
+            grid: "meter1", pv: "pv1", battery: "battery1"
+        }
     }
-  }
 
-  //Influx rename bucket -> database
-  let influx = household.influx
-  influx.database = influx.bucket
-  delete influx.bucket
-  evccConfig.influx = influx
+    //Influx rename bucket -> database
+    let influx = household.influx
+    influx.database = influx.bucket
+    delete influx.bucket
+    evccConfig.influx = influx
 
-  // custom meter
-  evccConfig.meters =
-    [{
-      name: "meter1",
-      type: "custom",
-      power: {
-        source: "http",
-        uri: `http://localhost:${household.consumptions[0].port}/meter/currentconsumption`,
-        method: "GET",
-        headers: {
-          'content-type': "application/json"
+    // custom meter
+    evccConfig.meters =
+        [{
+            name: "meter1",
+            type: "custom",
+            power: {
+                source: "http",
+                uri: `http://localhost:${household.consumptions[0].port}/meter/currentconsumption`,
+                method: "GET",
+                headers: {
+                    'content-type': "application/json"
+                },
+                scale: 1000, //We return kW
+                timeout: "10s"
+            }
         },
-        scale: 1000, //We return kW
-        timeout: "10s"
-      }
-    },
-      {
-        name: "pv1",
+            {
+                name: "pv1",
+                type: "custom",
+                power: {
+                    source: "http",
+                    uri: `http://localhost:${household.pvs[0].port}/pv/currentproduction`,
+                    method: "GET",
+                    headers: {
+                        'content-type': "application/json"
+                    },
+                    timeout: "10s",
+                    scale: 1000, //We return kW
+                }
+            },
+            {
+                name: "wallbox1",
+                type: "custom",
+                power: {
+                    source: "http",
+                    uri: `http://localhost:${household.wallboxes[0].port}/meter/currentpower`,
+                    method: "GET",
+                    headers: {
+                        'content-type': "application/json"
+                    },
+                    timeout: "10s",
+                    scale: 1000, //We return kW
+                }
+            },
+            {
+                name: "battery1",
+                type: "custom",
+                power: {
+                    source: "http",
+                    uri: `http://localhost:${household.batteries[0].port}/battery/currentpower`,
+                    method: "GET",
+                    headers: {
+                        'content-type': "application/json"
+                    },
+                    scale: 1000, //We return kW
+                    timeout: "10s"
+                },
+                soc: {
+                    source: "http",
+                    uri: `http://localhost:${household.batteries[0].port}/battery/soc`,
+                    method: "GET",
+                    headers: {
+                        'content-type': "application/json"
+                    },
+                    scale: 100, //We return a value from 0 to 1
+                    timeout: "10s"
+                },
+                energy: {
+                    source: "http",
+                    uri: `http://localhost:${household.batteries[0].port}/battery/energy`,
+                    method: "GET",
+                    headers: {
+                        'content-type': "application/json"
+                    },
+                    timeout: "10s"
+                },
+                capacity: household.evs[0].batterySizeInKWh,
+
+            }
+        ]
+
+    evccConfig.vehicles = [{
+        name: "vehicle1",
+        title: "Vehicle 1",
         type: "custom",
-        power: {
-          source: "http",
-          uri: `http://localhost:${household.pvs[0].port}/pv/currentproduction`,
-          method: "GET",
-          headers: {
-            'content-type': "application/json"
-          },
-          timeout: "10s",
-          scale: 1000, //We return kW
-        }
-      },
-      {
-        name: "wallbox1",
-        type: "custom",
-        power: {
-          source: "http",
-          uri: `http://localhost:${household.wallboxes[0].port}/meter/currentpower`,
-          method: "GET",
-          headers: {
-            'content-type': "application/json"
-          },
-          timeout: "10s",
-          scale: 1000, //We return kW
-        }
-      },
-      {
-        name: "battery1",
-        type: "custom",
-        power: {
-          source: "http",
-          uri: `http://localhost:${household.batteries[0].port}/battery/currentpower`,
-          method: "GET",
-          headers: {
-            'content-type': "application/json"
-          },
-          scale: 1000, //We return kW
-          timeout: "10s"
-        },
+        capacity: household.batteries[0].batterySizeInKWh,
         soc: {
-          source: "http",
-          uri: `http://localhost:${household.batteries[0].port}/battery/soc`,
-          method: "GET",
-          headers: {
-            'content-type': "application/json"
-          },
-          scale: 100, //We return a value from 0 to 1
-          timeout: "10s"
+            source: "http",
+            uri: `http://localhost:${household.evs[0].port}/vehicle/soc`,
+            method: "GET",
+            headers: {
+                'content-type': "application/json"
+            },
+            scale: 100, //We return a value from 0 to 1
+            timeout: "10s"
         },
-        energy: {
-          source: "http",
-          uri: `http://localhost:${household.batteries[0].port}/battery/energy`,
-          method: "GET",
-          headers: {
-            'content-type': "application/json"
-          },
-          timeout: "10s"
+        status: {
+            source: "http",
+            uri: `http://localhost:${household.evs[0].port}/vehicle/status`,
+            method: "GET",
+            headers: {
+                'content-type': "application/json"
+            },
+            timeout: "10s"
         },
-        capacity: household.evs[0].batterySizeInKWh,
+        range: {
+            source: "http",
+            uri: `http://localhost:${household.evs[0].port}/vehicle/range`,
+            method: "GET",
+            headers: {
+                'content-type': "application/json"
+            },
+            timeout: "10s"
+        }
+    }]
 
-      }
-    ]
+    evccConfig.chargers = [{
+        name: "charger1",
+        type: "custom",
+        status: {
+            source: "http",
+            uri: `http://localhost:${household.wallboxes[0].port}/charger/status`,
+            method: "GET",
+            headers: {
+                'content-type': "application/json"
+            },
+            timeout: "10s"
+        },
+        enabled: {
+            source: "http",
+            uri: `http://localhost:${household.wallboxes[0].port}/charger/enabled`,
+            method: "GET",
+            headers: {
+                'content-type': "application/json"
+            },
+            timeout: "10s"
+        },
+        enable: {
+            source: "http",
+            uri: `http://localhost:${household.wallboxes[0].port}/charger/enable`,
+            method: "POST",
+            headers: {
+                'content-type': 'text/plain'
+            },
+            timeout: "10s"
+        },
+        maxcurrent: {
+            source: "http",
+            uri: `http://localhost:${household.wallboxes[0].port}/charger/maxcurrent`,
+            method: "POST",
+            headers: {
+                'content-type': 'text/plain'
+            },
+            timeout: "10s"
+        },
+    }]
 
-  evccConfig.vehicles = [{
-    name: "vehicle1",
-    title: "Vehicle 1",
-    type: "custom",
-    capacity: household.batteries[0].batterySizeInKWh,
-    soc: {
-      source: "http",
-      uri: `http://localhost:${household.evs[0].port}/vehicle/soc`,
-      method: "GET",
-      headers: {
-        'content-type': "application/json"
-      },
-      scale: 100, //We return a value from 0 to 1
-      timeout: "10s"
-    },
-    status: {
-      source: "http",
-      uri: `http://localhost:${household.evs[0].port}/vehicle/status`,
-      method: "GET",
-      headers: {
-        'content-type': "application/json"
-      },
-      timeout: "10s"
-    },
-    range: {
-      source: "http",
-      uri: `http://localhost:${household.evs[0].port}/vehicle/range`,
-      method: "GET",
-      headers: {
-        'content-type': "application/json"
-      },
-      timeout: "10s"
+    evccConfig.network = {
+        port: household.port, schema: "http"
     }
-  }]
 
-  evccConfig.chargers = [{
-    name: "charger1",
-    type: "custom",
-    status: {
-      source: "http",
-      uri: `http://localhost:${household.wallboxes[0].port}/charger/status`,
-      method: "GET",
-      headers: {
-        'content-type': "application/json"
-      },
-      timeout: "10s"
-    },
-    enabled: {
-      source: "http",
-      uri: `http://localhost:${household.wallboxes[0].port}/charger/enabled`,
-      method: "GET",
-      headers: {
-        'content-type': "application/json"
-      },
-      timeout: "10s"
-    },
-    enable: {
-      source: "http",
-      uri: `http://localhost:${household.wallboxes[0].port}/charger/enable`,
-      method: "POST",
-      headers: {
-        'content-type': 'text/plain'
-      },
-      timeout: "10s"
-    },
-    maxcurrent: {
-      source: "http",
-      uri: `http://localhost:${household.wallboxes[0].port}/charger/maxcurrent`,
-      method: "POST",
-      headers: {
-        'content-type': 'text/plain'
-      },
-      timeout: "10s"
-    },
-  }]
+    evccConfig.loadpoints = [{
+        title: "Garage",
+        charger: "charger1",
+        vehicle: "vehicle1",
+        meter: "wallbox1"
+    }]
 
-  evccConfig.network = {
-    port: household.port, schema: "http"
-  }
-
-  evccConfig.loadpoints = [{
-    title: "Garage",
-    charger: "charger1",
-    vehicle: "vehicle1",
-    meter: "wallbox1"
-  }]
-
-  evccConfig.tariffs = {
-    currency: "EUR",
-    grid: {
-      type: "fixed", price: 0.35
+    evccConfig.tariffs = {
+        currency: "EUR",
+        grid: {
+            type: "fixed", price: 0.35
+        }
     }
-  }
 
-  let s = yaml.dump(evccConfig)
-  console.log(s)
+    let s = yaml.dump(evccConfig)
+    fs.writeFile(`evcc-configs/evcc-${household.name.replace(" ", "-")}.yml`, s, (err) => {
+        if (err) throw err
+    })
+
+    console.log(s)
 }
--- simulators/simulation-configs.js
+++ simulators/simulation-configs.js
@@ -1,45 +1,86 @@
 export default {
-  households: [
-    {
-      name: 'Household 1',
-      port: '7070',
-      pvs: [{
-        file: './production_values/pv_sim_export_4000_0_45.json',
-        port: 9001
-      }],
-      evs: [{
-        chargingEfficiency: 0.95,
-        maxChargeRateInKW: 22,
-        batterySizeInKWh: 50,
-        averageConsumptionPer100KM: 16,
-        initialSoCInKWh: 20,
-        locationFile: './car_values/House 2 1/Results/CarLocation.Car 2, 22kW Charging Power, avg. Speed 30 km h.HH1.json',
-        distanceFile: './car_values/House 2 1/Results/DrivingDistance.Car 2, 22kW Charging Power, avg. Speed 30 km h.HH1.json',
-        port: 9002
-      }],
-      wallboxes: [{
-        maxCurrent: 16, // in A
-        port: 9002
-      }],
-      batteries: [{
-        batterySizeInKWh: 10,
-        initSoCinKWh: 8,
-        maxDischargeRateInKW: 5,
-        maxChargeRateInKW: 4,
-        chargingEfficiency: 0.95,
-        dischargingEfficiency: 0.92,
-        port: 9004
-      }],
-      consumptions: [{
-        file: 'consumption_values/House_1-HH_1.json',
-        port: 9005
-      }],
-      influx: {
-        url: "http://localhost:8086",
-        bucket: "sim",
-        token: "zkF_MBAO4dcMB65cSmgz8LBmow7dUYa38iQtXmo9mAOR6Yw4oEGINN1I8PFNapL-3E4tNR9ABSZIT5XhZjcLyQ==",
-        org: "home"
-      }
-    }
-  ]
+    households: [
+        {
+            name: 'Household 1',
+            port: '7070',
+            pvs: [{
+                file: './production_values/pv_sim_export_4000_0_45.json',
+                port: 9001
+            }],
+            evs: [{
+                chargingEfficiency: 0.95,
+                maxChargeRateInKW: 22,
+                batterySizeInKWh: 50,
+                averageConsumptionPer100KM: 16,
+                initialSoCInKWh: 20,
+                locationFile: './car_values/House 2 1/Results/CarLocation.Car 2, 22kW Charging Power, avg. Speed 30 km h.HH1.json',
+                distanceFile: './car_values/House 2 1/Results/DrivingDistance.Car 2, 22kW Charging Power, avg. Speed 30 km h.HH1.json',
+                port: 9002
+            }],
+            wallboxes: [{
+                maxCurrent: 16, // in A
+                port: 9002
+            }],
+            batteries: [{
+                batterySizeInKWh: 10,
+                initSoCinKWh: 8,
+                maxDischargeRateInKW: 5,
+                maxChargeRateInKW: 4,
+                chargingEfficiency: 0.95,
+                dischargingEfficiency: 0.92,
+                port: 9004
+            }],
+            consumptions: [{
+                file: 'consumption_values/House_1-HH_1.json',
+                port: 9005
+            }],
+            influx: {
+                url: "http://localhost:8086",
+                bucket: "sim",
+                token: "zkF_MBAO4dcMB65cSmgz8LBmow7dUYa38iQtXmo9mAOR6Yw4oEGINN1I8PFNapL-3E4tNR9ABSZIT5XhZjcLyQ==",
+                org: "home"
+            }
+        },
+        {
+            name: 'Household 2',
+            port: '7071',
+            pvs: [{
+                file: './production_values/pv_sim_export_4000_0_45.json',
+                port: 9006
+            }],
+            evs: [{
+                chargingEfficiency: 0.95,
+                maxChargeRateInKW: 22,
+                batterySizeInKWh: 50,
+                averageConsumptionPer100KM: 16,
+                initialSoCInKWh: 20,
+                locationFile: './car_values/House 2 2/Results/CarLocation.Car 2, 22kW Charging Power, avg. Speed 30 km h.HH1.json',
+                distanceFile: './car_values/House 2 2/Results/DrivingDistance.Car 2, 22kW Charging Power, avg. Speed 30 km h.HH1.json',
+                port: 9007
+            }],
+            wallboxes: [{
+                maxCurrent: 16, // in A
+                port: 9007
+            }],
+            batteries: [{
+                batterySizeInKWh: 10,
+                initSoCinKWh: 8,
+                maxDischargeRateInKW: 5,
+                maxChargeRateInKW: 4,
+                chargingEfficiency: 0.95,
+                dischargingEfficiency: 0.92,
+                port: 9008
+            }],
+            consumptions: [{
+                file: 'consumption_values/House_1-HH_2.json',
+                port: 9009
+            }],
+            influx: {
+                url: "http://localhost:8086",
+                bucket: "sim",
+                token: "zkF_MBAO4dcMB65cSmgz8LBmow7dUYa38iQtXmo9mAOR6Yw4oEGINN1I8PFNapL-3E4tNR9ABSZIT5XhZjcLyQ==",
+                org: "home"
+            }
+        }
+    ]
 };
